e9a92d79fc17d434b401ec90d7911e9c86c2e3ef
Manisha-jaishwal <manishajaishwal411@gmail.com>
2025-11-28 18:16:07 +0530

    update changes

--- my-app/src/components/Hero/Hero.jsx
+++ my-app/src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
-import headphones from "../../assets/hero_headphones.png";
+import headphoneImg from "../../assets/hero_headphones.png";
 
 function Hero() {
   return (
@@ -9,7 +9,8 @@ function Hero() {
         <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousands podcast episodes</h1>
       </div>
-      <img src={headphones} width={212} alt="headphones" />
+
+      <img src={headphoneImg} width={212} alt="headphones" />
     </div>
   );
 }
